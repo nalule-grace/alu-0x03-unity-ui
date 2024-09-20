acd07f1207374cef6031297c993594f33d96a5f6
lanaarafat <a.mutoni1@alustudent.com>
2024-09-20 07:02:44 +0200

    UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,6 +11,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody Player;
     private int score = 0;
     public int health = 5;
+    public Text scoreText;
+    public Text healthText;
+    public Text winLoseText;
+    public Text winLoseBG;
 
     // Start is called before the first frame update
     void Start()
@@ -44,10 +49,20 @@ public class PlayerController : MonoBehaviour
             Player.AddForce(-speed, 0, 0);
         }
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("menu");
+        }
+
         if (health == 0)
         {
-            Debug.Log($"Gme Over!");
-            SceneManager.LoadScene("maze");
+            // Debug.Log($"Game Over!");
+            // SceneManager.LoadScene("maze");
+            winLoseText.text = $"Game Over!";
+            winLoseText.color = Color.white;
+            winLoseBG.color = Color.red;
+            winLoseBG.gameObject.SetActive(true);
+            StartCoroutine(LoadScene(3f));
         }
     }
 
@@ -56,20 +71,42 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Pickup"))
         {
             score++;
-            Debug.Log($"Score: {score}");
+            // Debug.Log($"Score: {score}");
+            SetScoreText();
             Destroy(other.gameObject);
         }
 
         if (other.CompareTag("Trap"))
         {
             health--;
-            Debug.Log($"Health: {health}");
+            // Debug.Log($"Health: {health}");
         }
 
         if (other.CompareTag("Goal"))
         {
-            Debug.Log($"You win!");
+            // Debug.Log($"You win!");
+            winLoseText.text = $"You Win!";
+            winLoseText.color = Color.black;
+            winLoseBG.color = Color.green;
+            winLoseBG.gameObject.SetActive(true);
+            StartCoroutine(LoadScene(3f));
         }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = $"Score: {score}";
+    }
+
+    void SetHealthText()
+    {
+        healthText.text = $"Health: {health}";
+    }
+
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        StartCoroutine(LoadScene(seconds));
     }
 
 }
